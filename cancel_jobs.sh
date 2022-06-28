d78af7feb242980b435f1e89c061e9700594202c
Yash Sandhu <hasush@gmail.com>
2022-06-28 12:52:07 -0400

    Update cancel_jobs.sh

--- cancel_jobs.sh
+++ cancel_jobs.sh
@@ -4,3 +4,11 @@ do
     echo -ne "Stopping job $job in state $state\t"
     aws batch terminate-job --reason "Terminating job." --job-id $job && echo "Done." || echo "Failed."
 done
+
+# aws stepfunctions list-executions \
+#   --state-machine-arn arn:aws:states:us-east-1:786346568665:stateMachine:listing_fraud_detection.user.yash.FraudDetectionGatorInferen-uthdthcqnejerriz \
+#   --status-filter RUNNING \
+#   --query "executions[*].{executionArn:executionArn}" \
+#   --output text | \
+# xargs -I {} aws stepfunctions stop-execution \
+#   --execution-arn {} 
